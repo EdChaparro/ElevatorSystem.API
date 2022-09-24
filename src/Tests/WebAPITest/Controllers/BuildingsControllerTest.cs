88ca6a46afdb3a9bc6a5b32dd8935793ee187404
Ed Chaparro <ed@IntrepidProducts.com>
2022-09-24 16:53:57 -0400

    Added Unit Test for Building Post API

--- src/Tests/WebAPITest/Controllers/BuildingsControllerTest.cs
+++ src/Tests/WebAPITest/Controllers/BuildingsControllerTest.cs
@@ -10,6 +10,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using IntrepidProducts.WebAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
@@ -19,8 +20,9 @@ namespace IntrepidProducts.WebApiTest.Controllers
     [TestClass]
     public class BuildingsControllerTest
     {
+        #region Get
         [TestMethod]
-        public void ShouldReturnAllBuildings()
+        public void ShouldGetAllBuildings()
         {
             var mockRequestHandlerProcessor = new Mock<IRequestHandlerProcessor>();
 
@@ -72,6 +74,60 @@ namespace IntrepidProducts.WebApiTest.Controllers
             var model = okObjectResult.Value as BuildingsModel;
             Assert.IsNotNull(model);
             Assert.AreEqual(2, model.Buildings.Count);
+        }
+        #endregion
+
+        [TestMethod]
+        public void ShouldReportSuccessfulPost()
+        {
+            var mockRequestHandlerProcessor = new Mock<IRequestHandlerProcessor>();
+
+            var request = new AddBuildingRequest();
+            var requestBlock = new RequestBlock();
+            requestBlock.Add(request);
+
+            var response = new EntityAddedResponse(request)
+            {
+                EntityId = new Guid()
+            };
+
+            var responseBlock = new ResponseBlock(requestBlock);
+            responseBlock.Add(response);
+
+            var expectedResponseBlock = responseBlock;
+
+            mockRequestHandlerProcessor.Setup
+                (x =>
+                    x.Process(It.IsAny<RequestBlock>()))
+                .Returns(expectedResponseBlock);
+
+            var mockLinkGenerator = new Mock<LinkGenerator>();
+
+            var controller = new BuildingsController
+                (mockRequestHandlerProcessor.Object, mockLinkGenerator.Object)
+                {
+                    ControllerContext = new ControllerContext
+                    {
+                        HttpContext = new DefaultHttpContext() //Needed for HATEOAS URI generation
+                    }
+                };
+
+            var actionResult = controller.Post(new BuildingName { Name = "Foo" });
+
+            var createdAtActionResult = actionResult as CreatedAtActionResult;
+            Assert.IsNotNull(createdAtActionResult);
+
+            Assert.AreEqual(StatusCodes.Status201Created, createdAtActionResult.StatusCode);
+
+            var actualResults = createdAtActionResult.Value as BuildingName;
+            Assert.IsNotNull(actualResults);
+
+            var routeValues = createdAtActionResult.RouteValues;
+
+            Assert.AreEqual("Get", createdAtActionResult.ActionName);
+            Assert.AreEqual(1, routeValues.Count);
+            Assert.AreEqual("id", routeValues.Keys.First());
+            Assert.AreEqual(response.EntityId, routeValues.Values.First());
         }
     }
 }
