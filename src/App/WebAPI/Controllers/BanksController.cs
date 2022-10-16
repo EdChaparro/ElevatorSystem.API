0d081d27749a75631c4fc68c13278d67f6a73f12
Ed Chaparro <ed@IntrepidProducts.com>
2022-10-15 23:34:03 -0400

    Banks Controller placeholder

---
+++ src/App/WebAPI/Controllers/BanksController.cs
@@ -0,0 +1,19 @@
+﻿using IntrepidProducts.RequestResponseHandler.Handlers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace IntrepidProducts.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/v1/building/{id}/[controller]")]
+    [Produces("application/json")]
+    public class BanksController : AbstractApiController
+    {
+        public BanksController
+        (IRequestHandlerProcessor requestHandlerProcessor,
+            LinkGenerator linkGenerator)
+            : base(requestHandlerProcessor, linkGenerator)
+        {
+        }
+    }
+}
