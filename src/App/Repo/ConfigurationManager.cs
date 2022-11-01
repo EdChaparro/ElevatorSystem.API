5e55ce22636f1e8d7a03fc9b246bdf260b181c41
Ed Chaparro <ed@IntrepidProducts.com>
2022-11-01 15:01:19 -0400

    Added rudimentary Repo Configuration Manager

---
+++ src/App/Repo/ConfigurationManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IntrepidProducts.Repo
+{
+    public class ConfigurationManager
+    {
+        public string? DatabaseName { get; set; }
+
+        public void ValidateConfiguration()
+        {
+            if (string.IsNullOrEmpty(DatabaseName))
+            {
+                throw new ArgumentException("DatabaseName not specified");
+            }
+        }
+    }
+}
