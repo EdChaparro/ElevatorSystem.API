9f5e2068bbfc94ec286237472fe205a55e2e724a
Ed Chaparro <ed@IntrepidProducts.com>
2022-11-01 23:40:47 -0400

    Abandoned FileContextCore EF Strategy
    unable to resolve conflicting assembly dependency
    versioning, will roll a custom persistence solution.

---
+++ src/App/Repo/AbstractRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace IntrepidProducts.Repo
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        int Create(TEntity entity);
+        int Update(TEntity entity);
+        int Delete(TEntity entity);
+        TEntity? FindById(Guid id);
+    }
+
+    public abstract class AbstractRepo<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        protected AbstractRepo(ConfigurationManager configManager)
+        {
+            ConfigurationManager = configManager;
+        }
+
+        protected ConfigurationManager ConfigurationManager { get; }
+
+        public abstract int Create(TEntity entity);
+
+        public abstract int Update(TEntity entity);
+
+        public abstract int Delete(TEntity entity);
+
+        public abstract TEntity? FindById(Guid id);
+    }
+}
