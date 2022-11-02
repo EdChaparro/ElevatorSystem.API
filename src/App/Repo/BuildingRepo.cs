78f9900b54dde0f0899eb1f8ad2789e63cc0a38e
Ed Chaparro <ed@IntrepidProducts.com>
2022-11-02 17:38:09 -0400

    Introduce Building Repo

---
+++ src/App/Repo/BuildingRepo.cs
@@ -0,0 +1,16 @@
+﻿using IntrepidProducts.ElevatorSystem;
+using System.Collections.Generic;
+
+namespace IntrepidProducts.Repo
+{
+    public class BuildingRepo : AbstractFileRepo<Building>, IFindAll<Building>
+    {
+        public BuildingRepo(RepoConfigurationManager configManager) : base(configManager)
+        { }
+
+        public IEnumerable<Building> FindAll()
+        {
+            return FindAllEntities();
+        }
+    }
+}
