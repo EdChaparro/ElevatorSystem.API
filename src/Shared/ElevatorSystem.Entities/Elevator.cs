1f906fcba52bae5747cf77a66f98f84baa693551
Ed Chaparro <ed@IntrepidProducts.com>
2023-01-25 21:48:05 -0500

    Added Elevator Shared Entity
    for persistence

---
+++ src/Shared/ElevatorSystem.Entities/Elevator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IntrepidProducts.Common;
+
+namespace IntrepidProducts.Shared.ElevatorSystem.Entities
+{
+    public class Elevator : IHasId
+    {
+        public Elevator(Guid bankId, IntrepidProducts.ElevatorSystem.Elevators.Elevator elevator)
+        {
+            Id = elevator.Id;
+            BankId = bankId;
+
+            Name = elevator.Name;
+
+            FloorNbrs = elevator.OrderedFloorNumbers.ToList();
+            CurrentFloorNumber = elevator.CurrentFloorNumber;
+
+            IsEnabled = elevator.IsEnabled;
+            IsIdle = elevator.IsIdle;
+        }
+        public Elevator()
+        {
+            FloorNbrs = new List<int>();
+        }
+
+        public Guid Id { get; set; }
+
+        public Guid BankId { get; set; }
+
+        public string? Name { get; set; }
+
+        public int CurrentFloorNumber { get; set; }
+        public List<int> FloorNbrs { get; set; }
+
+        public bool IsEnabled { get; set; }
+        public bool IsIdle { get; set; }
+    }
+}
