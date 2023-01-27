82c3678c0b163e5f6cc7efe40ae673814fd55a15
Ed Chaparro <ed@IntrepidProducts.com>
2023-01-26 20:03:27 -0500

    Add Find Elevator Request Handler

--- src/App/Biz/Mappers/ElevatorMapper.cs
+++ src/App/Biz/Mappers/ElevatorMapper.cs
@@ -6,27 +6,42 @@ namespace IntrepidProducts.Biz.Mappers
 {
     public static class ElevatorMapper
     {
-        internal static List<ElevatorDTO> Map(IEnumerable<Elevator> elevators)
+        public static List<ElevatorDTO> Map(IEnumerable<Elevator> elevators)
         {
             var elevatorDTOs = new List<ElevatorDTO>();
 
             foreach (var elevator in elevators)
             {
-                var dto = new ElevatorDTO
+                var dto = Map(elevator);
+
+                if (dto == null)
                 {
-                    BankId = elevator.BankId,
-                    Id = elevator.Id,
-                    Name = elevator.Name,
-                    FloorNbrs = elevator.FloorNbrs,
-                    CurrentFloorNumber = elevator.CurrentFloorNumber,
-                    IsEnabled = elevator.IsEnabled,
-                    IsIdle = elevator.IsIdle
-                };
+                    continue;
+                }
 
                 elevatorDTOs.Add(dto);
             }
 
             return elevatorDTOs;
+        }
+
+        public static ElevatorDTO? Map(Elevator? elevator)
+        {
+            if (elevator == null)
+            {
+                return null;
+            }
+
+            return new ElevatorDTO
+            {
+                BankId = elevator.BankId,
+                Id = elevator.Id,
+                Name = elevator.Name,
+                FloorNbrs = elevator.FloorNbrs,
+                CurrentFloorNumber = elevator.CurrentFloorNumber,
+                IsEnabled = elevator.IsEnabled,
+                IsIdle = elevator.IsIdle
+            };
         }
     }
 }
---
+++ src/Tests/BizTest/RequestHandlers/Elevators/FindElevatorRequestHandlerTest.cs
@@ -0,0 +1,67 @@
+﻿using IntrepidProducts.ElevatorSystem.Banks;
+using IntrepidProducts.Repo;
+using IntrepidProducts.Shared.ElevatorSystem.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Linq;
+using IntrepidProducts.Biz.RequestHandlers.Elevators;
+using IntrepidProducts.ElevatorSystem.Shared.Requests.Elevators;
+
+namespace IntrepidProducts.BizTest.RequestHandlers.Elevators
+{
+    [TestClass]
+    public class FindElevatorRequestHandlerTest
+    {
+        [TestMethod]
+        public void ShouldReturnById()
+        {
+            //Setup
+            var mockRepo = new Mock<IRepository<BuildingElevatorBank>>();
+
+            const int NUMBER_OF_ELEVATORS = 2;
+
+            var bank = new Bank(NUMBER_OF_ELEVATORS, 1..10) { Name = "Bank A" };
+
+            var elevatorBank = new BuildingElevatorBank(Guid.NewGuid(), bank);
+
+            mockRepo.Setup(x =>
+                    x.FindById(bank.Id))
+                .Returns(elevatorBank);
+
+            var requestHandler = new FindElevatorRequestHandler(mockRepo.Object);
+
+            var findResponse = requestHandler
+                .Handle(new FindElevatorRequest
+                {
+                    BankId = bank.Id,
+                    ElevatorId = bank.Elevators.First().Id
+                });
+
+            //Assert
+            Assert.IsTrue(findResponse.IsSuccessful);
+            Assert.IsNotNull(findResponse.Entity);
+            Assert.AreEqual(bank.Elevators.First().Id, findResponse.Entity.Id);
+            Assert.AreEqual(NUMBER_OF_ELEVATORS, bank.Elevators.Count());
+        }
+
+        [TestMethod]
+        public void ShouldReturnNullWhenNotFound()
+        {
+            var mockRepo = new Mock<IRepository<BuildingElevatorBank>>();
+
+            var requestHandler = new FindElevatorRequestHandler(mockRepo.Object);
+
+            var findResponse = requestHandler
+                .Handle(new FindElevatorRequest
+                {
+                    BankId = Guid.NewGuid(),
+                    ElevatorId = Guid.NewGuid()
+                });
+
+            Assert.IsTrue(findResponse.IsSuccessful);
+
+            Assert.IsNull(findResponse.Entity);
+        }
+    }
+}
