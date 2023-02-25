8f779acbc14749832b6c130ecb5a26109d7e3520
Ed Chaparro <ed@IntrepidProducts.com>
2023-02-25 15:13:45 -0500

    Renamed project, Service -> ElevatorSystemService

--- src/App/Service/BankWorker.cs
+++ src/App/Service/BankWorker.cs
@@ -1,10 +1,10 @@
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using System;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace Service
+namespace IntrepidProducts.ElevatorSystemService
 {
     public class BankWorker : BackgroundService
     {
