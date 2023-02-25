225fb24b7bf3fb9baa0c2c4357a6e80c68aac15d
Ed Chaparro <ed@IntrepidProducts.com>
2023-02-25 10:53:54 -0500

    Add Start Bank Request and Response classes
    Intended to start a Bank Engine.

---
+++ src/Shared/ElevatorSystem.Shared/Responses/BankControlPanelResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using IntrepidProducts.ElevatorSystem.Shared.DTOs.Elevators;
+using IntrepidProducts.RequestResponse.Requests;
+using IntrepidProducts.RequestResponse.Responses;
+
+namespace IntrepidProducts.ElevatorSystem.Shared.Responses
+{
+    public class BankControlPanelResponse : ResponseAbstract
+    {
+        public BankControlPanelResponse(IRequest originalRequest, ErrorInfo? errorInfo = null)
+            : base(originalRequest, errorInfo)
+        { }
+
+        public bool IsRunning { get; set; }
+        public List<ElevatorDTO> ElevatorDTOs { get; set; } = new List<ElevatorDTO>();
+    }
+}
