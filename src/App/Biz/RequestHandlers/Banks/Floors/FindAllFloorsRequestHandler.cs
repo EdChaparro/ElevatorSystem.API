f41be5585f837ac81028850b6434c3ae99075d32
Ed Chaparro <ed@IntrepidProducts.com>
2023-08-05 15:17:28 -0400

    Add Find All Floors Request Handler
    Returns Floor DTOs for a given Bank ID

---
+++ src/App/Biz/RequestHandlers/Banks/Floors/FindAllFloorsRequestHandler.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using IntrepidProducts.ElevatorService.Banks;
+using IntrepidProducts.ElevatorSystem.Banks;
+using IntrepidProducts.ElevatorSystem.Shared.DTOs.Banks.Floors;
+using IntrepidProducts.ElevatorSystem.Shared.Requests.Banks.Floors;
+using IntrepidProducts.ElevatorSystem.Shared.Responses;
+using IntrepidProducts.ElevatorSystemBiz.Mappers;
+using IntrepidProducts.Repo;
+using IntrepidProducts.RequestResponseHandler.Handlers;
+
+namespace IntrepidProducts.ElevatorSystemBiz.RequestHandlers.Banks.Floors
+{
+    public class FindAllFloorsRequestHandler :
+        AbstractRequestHandler<FindAllFloorsRequest, FindEntitiesResponse<FloorDTO>>
+    {
+        public FindAllFloorsRequestHandler
+            (IBuildingElevatorBankRepository bankRepo,
+                IBankServiceRegistry bankServiceRegistry)
+
+        {
+            _bankRepo = bankRepo;
+            _bankServiceRegistry = bankServiceRegistry;
+        }
+
+        private readonly IBuildingElevatorBankRepository _bankRepo;
+        private readonly IBankServiceRegistry _bankServiceRegistry;
+
+        protected override FindEntitiesResponse<FloorDTO> DoHandle(FindAllFloorsRequest request)
+        {
+            var banks =
+                _bankRepo.FindByBuildingId(request.BuildingId).ToList();
+
+            var bank = banks.FirstOrDefault(x => x.Id == request.BankId);
+
+            var response = new FindEntitiesResponse<FloorDTO>(request);
+
+            if (bank == null)
+            {
+                return response;
+            }
+
+            var bankService = _bankServiceRegistry.Get(bank.Id);
+
+            if (bankService == null)
+            {
+                var defaultDTO = FloorMapper
+                    .Map(new Bank(bank.NumberOfElevators, bank.FloorNbrs.ToArray()));
+                return new FindEntitiesResponse<FloorDTO>(request) { Entities = defaultDTO };
+            }
+
+            var bankEntity = bankService.Entity;
+
+            var dtos = FloorMapper
+                .Map(new Bank(bank.NumberOfElevators, bank.FloorNbrs.ToArray()));
+
+
+            foreach (var dto in dtos)
+            {
+                var floorInfo = bankEntity.GetFloorFor(dto.Number);
+                var panel = floorInfo?.Panel;
+                if (panel == null)
+                {
+                    continue;
+                }
+
+                var downButton = panel.DownButton;
+                var upButton = panel.UpButton;
+
+                dto.IsDownCallBackRequested = downButton?.IsPressed ?? false;
+                dto.IsUpCallBackRequested = upButton?.IsPressed ?? false;
+            }
+
+            return new FindEntitiesResponse<FloorDTO>(request) { Entities = dtos };
+        }
+    }
+}
---
+++ src/Tests/BizTest/RequestHandlers/Banks/Floors/FindAllFloorsRequestHandlerTest.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using IntrepidProducts.ElevatorService;
+using IntrepidProducts.ElevatorService.Banks;
+using IntrepidProducts.ElevatorSystem.Banks;
+using IntrepidProducts.ElevatorSystem.Elevators;
+using IntrepidProducts.ElevatorSystem.Shared.Requests.Banks.Floors;
+using IntrepidProducts.ElevatorSystemBiz.RequestHandlers.Banks.Floors;
+using IntrepidProducts.Repo;
+using IntrepidProducts.Shared.ElevatorSystem.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace IntrepidProducts.ElevatorSystemBizTest.RequestHandlers.Banks.Floors
+{
+    [TestClass]
+    public class FindAllFloorsRequestHandlerTest
+    {
+        [TestMethod]
+        public void ShouldReturnAllBankFloors()
+        {
+            //Setup
+            var buildingId = Guid.NewGuid();
+            var bank1 = new BuildingElevatorBank(buildingId, new Bank(2, 1..5));
+            var bank2 = new BuildingElevatorBank(buildingId, new Bank(2, 6..4));
+
+            var request = new FindAllFloorsRequest
+            {
+                BuildingId = buildingId,
+                BankId = bank2.Id
+            };
+
+            var mockBankRepo = new Mock<IBuildingElevatorBankRepository>();
+            mockBankRepo.Setup(x =>
+                    x.FindByBuildingId(request.BuildingId))
+                .Returns(new List<BuildingElevatorBank> { bank1, bank2 });
+
+            var mockBankServiceRegistry = new Mock<IBankServiceRegistry>();
+
+            var findAllFloorsRequestHandler = new FindAllFloorsRequestHandler
+                (mockBankRepo.Object, mockBankServiceRegistry.Object);
+
+            var findResponse = findAllFloorsRequestHandler.Handle(request);
+
+            //Assert
+            Assert.IsTrue(findResponse.IsSuccessful);
+
+            var floorsReturned = findResponse.Entities;
+            Assert.AreEqual(4, floorsReturned.Count);
+
+            var bankFloorNbr = bank2.LowestFloorNbr;
+
+            for (var i = 0; i < bank2.FloorNbrs.Count; i++)
+            {
+                var floorDTO = floorsReturned[i];
+
+                Assert.AreEqual(bankFloorNbr, floorDTO.Number);
+                Assert.IsFalse(floorDTO.IsDownCallBackRequested);
+                Assert.IsFalse(floorDTO.IsUpCallBackRequested);
+
+                bankFloorNbr++;
+            }
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectElevatorCallBackStatus()
+        {
+            //Setup
+            var buildingId = Guid.NewGuid();
+
+            var bankEntity2 = new Bank(2, 6..4);
+
+            var bank1 = new BuildingElevatorBank(buildingId, new Bank(2, 1..5));
+            var bank2 = new BuildingElevatorBank(buildingId, bankEntity2);
+
+            Assert.IsTrue(bankEntity2.PressButtonForFloorNumber(7, Direction.Up));
+            Assert.IsTrue(bankEntity2.PressButtonForFloorNumber(9, Direction.Down));
+
+            var request = new FindAllFloorsRequest
+            {
+                BuildingId = buildingId,
+                BankId = bank2.Id
+            };
+
+            var mockBankService = new Mock<IEntityBackgroundService<Bank>>();
+            mockBankService.Setup(x =>
+                    x.Entity)
+                .Returns(bankEntity2);
+
+            var mockBankRepo = new Mock<IBuildingElevatorBankRepository>();
+            mockBankRepo.Setup(x =>
+                    x.FindByBuildingId(request.BuildingId))
+                .Returns(new List<BuildingElevatorBank> { bank1, bank2 });
+
+            var mockBankServiceRegistry = new Mock<IBankServiceRegistry>();
+            mockBankServiceRegistry.Setup(x =>
+                    x.Get(bank2.Id))
+                .Returns(mockBankService.Object);
+
+            var findAllFloorsRequestHandler = new FindAllFloorsRequestHandler
+                (mockBankRepo.Object, mockBankServiceRegistry.Object);
+
+            var findResponse = findAllFloorsRequestHandler.Handle(request);
+
+            //Assert
+            Assert.IsTrue(findResponse.IsSuccessful);
+
+            var floorsReturned = findResponse.Entities;
+            Assert.AreEqual(4, floorsReturned.Count);
+
+            var bankFloorNbr = bank2.LowestFloorNbr;
+
+            for (var i = 0; i < bank2.FloorNbrs.Count; i++)
+            {
+                var floorDTO = floorsReturned[i];
+
+                Assert.AreEqual(bankFloorNbr, floorDTO.Number);
+
+                switch (bankFloorNbr)
+                {
+                    case 7:
+                        Assert.IsFalse(floorDTO.IsDownCallBackRequested);
+                        Assert.IsTrue(floorDTO.IsUpCallBackRequested);
+                        bankFloorNbr++;
+                        continue;
+                    case 9:
+                        Assert.IsTrue(floorDTO.IsDownCallBackRequested);
+                        Assert.IsFalse(floorDTO.IsUpCallBackRequested);
+                        bankFloorNbr++;
+                        continue;
+                    default:
+                        Assert.IsFalse(floorDTO.IsDownCallBackRequested);
+                        Assert.IsFalse(floorDTO.IsUpCallBackRequested);
+                        bankFloorNbr++;
+                        break;
+                }
+            }
+        }
+
+        [TestMethod]
+        public void ShouldHandleNotFound()
+        {
+            //Setup
+            var buildingId = Guid.NewGuid();
+            var bank = new BuildingElevatorBank(buildingId, new Bank(2, 6..4));
+
+            var request = new FindAllFloorsRequest
+            {
+                BuildingId = buildingId,
+                BankId = bank.Id
+            };
+
+            var mockBankRepo = new Mock<IBuildingElevatorBankRepository>();
+            mockBankRepo.Setup(x =>
+                    x.FindByBuildingId(request.BuildingId))
+                .Returns(new List<BuildingElevatorBank> ());
+
+            var mockBankServiceRegistry = new Mock<IBankServiceRegistry>();
+
+            var findAllFloorsRequestHandler = new FindAllFloorsRequestHandler
+                (mockBankRepo.Object, mockBankServiceRegistry.Object);
+
+            var findResponse = findAllFloorsRequestHandler.Handle(request);
+
+            //Assert
+            Assert.IsFalse(findResponse.Entities.Any());
+        }
+    }
+}
